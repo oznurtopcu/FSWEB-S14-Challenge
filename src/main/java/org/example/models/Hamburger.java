a9b5180497bde41f553bc9be94cccd4d76fb0652
oznurtopcu <topcuoznur121@gmail.com>
2025-03-15 15:13:02 +0300

    create hamburger class

---
+++ src/main/java/org/example/models/Hamburger.java
@@ -0,0 +1,62 @@
+package org.example.models;
+
+public class Hamburger {
+    private String name;
+    private String meat;
+    private double price;
+    private String breadRollType;
+    private String addition1Name;
+    private double addition1Price;
+    private String addition2Name;
+    private double addition2Price;
+    private String addition3Name;
+    private double addition3Price;
+    private String addition4Name;
+    private double addition4Price;
+
+    public Hamburger(String name, String meat, double price, String breadRollType) {
+        this.name = name;
+        this.meat = meat;
+        this.price = price;
+        this.breadRollType = breadRollType;
+    }
+
+    public String getName() {
+        return name;
+    }
+    public String getMeat() {
+        return meat;
+    }
+    public double getPrice() {
+        return price + addition1Price + addition2Price + addition3Price + addition4Price;
+    }
+
+    public String getBreadRollType() {
+        return breadRollType;
+    }
+
+    public void addHamburgerAddition1(String name, double price) {
+        this.addition1Name = name;
+        this.addition1Price = price;
+    }
+
+    public void addHamburgerAddition2(String name, double price) {
+        this.addition2Name = name;
+        this.addition2Price = price;
+    }
+
+    public void addHamburgerAddition3(String name, double price) {
+        this.addition3Name = name;
+        this.addition3Price = price;
+    }
+
+    public void addHamburgerAddition4(String name, double price) {
+        this.addition4Name = name;
+        this.addition4Price = price;
+    }
+
+    public void itemizeHamburger() {
+        System.out.println("Hamburger: " + name + "Et: "+ meat + "Ekmek Tipi: "+ breadRollType + "Malzemeler: " + addition1Name + " , " + addition2Name + " , " + addition3Name + " , " + addition4Name + "  Toplam Fiyat: " + (price + addition1Price + addition2Price + addition3Price + addition4Price));
+    }
+
+}
